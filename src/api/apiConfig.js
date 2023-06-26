92f3263186b9977a7c41bc50ac4143dc175c079b
unknown <yunikashakya082@gmail.com>
2023-06-26 08:41:36 +0545

    added madal and slider components

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,5 +1,5 @@
 const apiConfig={
-    baseUrl: 'https://www.movienpl.com',
+    baseUrl: 'https://api.themoviedb.org/3/',
     apiKey: '8b5bc89c3aa94911f33a526d5c7fdf97',
     originalImage: (imgPath)=> `https://image.tmdb.org/t/p/original/${imgPath}`,
     w500Image: (imgPath)=> `https://image.tmdb.org/t/p/w500/${imgPath}`
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import HeroSlide from '../hero slide/HeroSlide'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+      <HeroSlide />
+    </>
   )
 }
 
